f603ad45fec042263f02df7c452e367d1cfe7c53
Rainbough Phillips <rainbough@gmail.com>
2013-10-17 09:19:42 -0500

    stuff from yesterday

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
 
+gem 'nokogiri', '~> 1.5.10'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
---
+++ app/mailers/notifier.rb
@@ -0,0 +1,17 @@
+class Notifier < ActionMailer::Base
+  default from: "rainbough@gmail.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notifier.subscription_confirmation.subject
+  #
+  def subscription_confirmation(user, project)
+    @user = user
+    @project = project
+
+    # Change the 'to:' section to your partner's name and email
+    mail( to: "#{user.name} <#{user.email}>",
+          subject: "Subscribed to #{project.name.capitalize} | Ativa")
+  end
+end
